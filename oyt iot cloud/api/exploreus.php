fbbed85daa61e356dd157471c9e3a6013684d2d3
rockstartushar <tdangayach6@gmail.com>
2021-04-14 08:24:01 +0530

    home page desig

--- oyt iot cloud/api/exploreus.php
+++ oyt iot cloud/api/exploreus.php
@@ -5,7 +5,8 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="stylesheet" href="./../assets/css/main.css" />
-		<link rel="stylesheet" href="./../assets/css/exploreus.css" />
+		<link rel="stylesheet" href="./../assets/css/home.css" />
+		<link rel="stylesheet" href="./../assets/css/exploreus.css" />		
 	</head>
 	<body>
 	<!-- Navbar -->
@@ -53,50 +54,56 @@
             <a class="nav-items" href="signup-user.php">Sign Up</a>
         </div>
     </div>
-    <br>
+	<!-- Explore Us -->
+    <div class="banner">
+        <div class="bannertxt">
+            <h1>Explore <br> our <br> IoT Cloud <span> Platform</span></h1>
+        </div>
+    </div>
     	<!-- Projects -->
 		<div class="sec">
 			<h2>Projects</h2>
+			<hr>
 			<div class="project">
 				<div id="1" class="project-card">
 						<img src="./../images/banner.jpg" alt="">
 					<div id="description">
-						<h3>fedfdd</h3>
+						<h3>Heading 1</h3>
 						<p>sdshjflefn</p>
 					</div>
 				</div>
 				<div id="2" class="project-card">
 						<img src="./../images/banner.jpg" alt="">
 					<div id="description">
-						<h3>fedfdd</h3>
+						<h3>Heading 2</h3>
 						<p>sdshjflefn</p>
 					</div>
 				</div>
 				<div id="3" class="project-card">
 						<img src="./../images/banner.jpg" alt="">
 					<div id="description">
-						<h3>fedfdd</h3>
+						<h3>Heading 3</h3>
 						<p>sdshjflefn</p>
 					</div>
 				</div>
 				<div id="4" class="project-card">
 						<img src="./../images/banner.jpg" alt="">
 					<div id="description">
-						<h3>fedfdd</h3>
+						<h3>Heading 4</h3>
 						<p>sdshjflefn</p>
 					</div>
 				</div>
 				<div id="5" class="project-card">
 						<img src="./../images/banner.jpg" alt="">
 					<div id="description">
-						<h3>fedfdd</h3>
+						<h3>Heading 5</h3>
 						<p>sdshjflefn</p>
 					</div>
 				</div>
 				<div id="6" class="project-card">
 						<img src="./../images/banner.jpg" alt="">
 					<div id="description">
-						<h3>fedfdd</h3>
+						<h3>Heading 6</h3>
 						<p>sdshjflefn</p>
 					</div>
 				</div>
@@ -105,29 +112,41 @@
 		<!-- Clients -->
 		<div class="sec">
 			<h2>Our Clients</h2>
+			<hr>
 			<div class="client">
-				<img src="./../images/banner.jpg" alt="">
-				<img src="./../images/banner.jpg" alt="">
-				<img src="./../images/banner.jpg" alt="">
-				<img src="./../images/banner.jpg" alt="">
-			</div>
+			<img src="./../images/icon/clientlogo.png" alt="">
+			<img src="./../images/icon/clientlogo.png" alt="">
+			<img src="./../images/icon/clientlogo.png" alt="">
+			<img src="./../images/icon/clientlogo.png" alt="">
+			<img src="./../images/icon/clientlogo.png" alt="">
+			<img src="./../images/icon/clientlogo.png" alt="">
+			<img src="./../images/icon/clientlogo.png" alt="">
+			<img src="./../images/icon/clientlogo.png" alt="">
+			<img src="./../images/icon/clientlogo.png" alt="">
+			<img src="./../images/icon/clientlogo.png" alt="">	
+			<img src="./../images/icon/clientlogo.png" alt="">
+			<img src="./../images/icon/clientlogo.png" alt="">
+			<img src="./../images/icon/clientlogo.png" alt="">
+			<img src="./../images/icon/clientlogo.png" alt="">
+			<img src="./../images/icon/clientlogo.png" alt="">
+			<img src="./../images/icon/clientlogo.png" alt="">
+		</div>
 		</div>
 		<div class="contact sec">
-			<h2>Contact Us</h2>
+			<hr>
 			<div class="contact-view">
-				<table>
-					<tr>
-						<th>Address</th>
-						<th>Mobile</th>
-						<th>Email</th>
-					</tr>
-					<tr>
-						<td><address>83/57, Sector 8, Pratap Nagar, Jaipur, Rajasthan - 302033</address><br />
-						<div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=83/57, Sector 8, Pratap Nagar  Jaipur, Rajasthan - 302033&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://www.embedmymap.com/">Embed My Map</a></div><style>.mapouter{position:relative;text-align:right;width:600px;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:300px;height:200px;}.gmap_iframe {width:600px!important;height:400px!important;}</style></div></td>
-						<td><a href="tel:+91-8285524418">+91-8285524418</a></td>
-						<td><a href="mailto:abc@gmail.com">contact@oytechnology.com</a></td>
-					</tr>
-				</table>
+				<div class="address">
+					<div class="footerlogo">
+            			<img width="100%" src="./../images/logo.png" alt="OYT IOT Cloud">
+        			</div>
+					<address>83/57, Sector 8, Pratap Nagar, Jaipur, Rajasthan - 302033</address><br />
+					<div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=83/57, Sector 8, Pratap Nagar  Jaipur, Rajasthan - 302033&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://www.embedmymap.com/">Embed My Map</a></div><style>.mapouter{position:relative;text-align:right;width:600px;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:300px;height:200px;}.gmap_iframe {width:600px!important;height:400px!important;}</style></div></td>
+				</div>
+				<div class="contactdetails">
+					<p>Contact Us</p>
+					<a href="tel:+91-8285524418">+91-8285524418</a>
+					<a href="mailto:abc@gmail.com">contact@oytechnology.com</a>
+				</div>
 				<form class="queryform" action="">
 					<label for="email">Email</label>
 					<input type="email" name="email" id="">
@@ -145,6 +164,6 @@
 			<script src="./../assets/js/skel.min.js"></script>
 			<script src="./../assets/js/util.js"></script>
 			<script src="./../assets/js/main.js"></script>
-			<script src="./../assets/js/home.js"></script>
+			<script src="./../assets/js/explore.js"></script>
 	</body>
 </html>
